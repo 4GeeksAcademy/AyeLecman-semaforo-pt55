ee3d7f8c3302fe7e808993c59ac9feebce73f896
Aye Lecman <ayelenlecman@gmail.com>
2025-07-17 00:21:32 +0000

    creacion del semaforo con react

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [redShadow, setRedShadow] = useState("none");
+	const [yellowShadow, setYellowShadow] = useState("none"); 
+	const [greenShadow, setGreenShadow] = useState("none");    
+
+	const red = () => {
+		if (redShadow == "none"){
+			setRedShadow ("0 0 20px 10px rgba(255, 0, 0, 0.6)")
+			setYellowShadow ("none")
+			setGreenShadow("none")
+		} else {
+			setRedShadow("none")
+		}
+	}
+
+	const yellow = () => {
+		if (yellowShadow == "none"){
+			setYellowShadow ("0 0 20px 10px rgba(255, 255, 0, 0.6)")
+			setRedShadow ("none")
+			setGreenShadow("none")
+		} else {
+			setYellowShadow("none")
+		}
+	}
+
+	const green = () => {
+		if (greenShadow == "none"){
+			setGreenShadow ("0 0 20px 10px rgba(0, 255, 0, 0.6)")
+			setRedShadow ("none")
+			setYellowShadow("none")
+		} else {
+			setGreenShadow("none")
+		}
+	}
+
+	return (
+		
+		<div className="d-flex flex-column justify-content-center align-items-center">
+			<div className="bg-dark" style={{ width: "1rem", height: "15vh"}}></div>
+			<div className="p-4 bg-dark rounded-4">
+				<div className="btn-group-vertical btn-group-lg gap-4" role="group" aria-label="Basic mixed styles example">
+					<button onClick={red} type="button" className="btn btn-danger rounded-circle" style={{ width: "4rem", height: "4rem", boxShadow: redShadow}}></button>
+					<button onClick={yellow} type="button" className="btn btn-warning rounded-circle" style={{ width: "4rem", height: "4rem", boxShadow: yellowShadow }}></button>
+					<button onClick={green} type="button" className="btn btn-success rounded-circle" style={{ width: "4rem", height: "4rem", boxShadow: greenShadow }}></button>
+				</div>
+			</div>
 		</div>
 	);
 };
